1d9a3af7d4c7e5bdac78b6e9cb9c594777e82093
sssmi <simeon.kerkola@gmail.com>
2018-04-15 09:35:52 +0300

    Initialize files

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,19 +1,9 @@
 import React, { Component } from 'react';
 
-class App extends Component {
+class LandingPage extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src="/logo.svg" className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    return <div>Test</div>;
   }
 }
 
-export default App;
+export default LandingPage;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './styles/index.css';
 import LandingPage from './components/LandingPage';
 import registerServiceWorker from './registerServiceWorker';
 
