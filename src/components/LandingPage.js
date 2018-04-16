8d8a62002238333c628e4bd9443e79062619c836
sssmi <simeon.kerkola@gmail.com>
2018-04-16 08:31:16 +0300

    Build overall grid layout

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
+import Sidebar from './Sidebar';
+import Header from './Header';
 
 class LandingPage extends Component {
   render() {
-    return <div>Test</div>;
+    return (
+      <div className="container">
+        <Sidebar />
+        <Header />
+        <div className="realtors">Top 3 realtors</div>
+        <section className="features">Features</section>
+        <div className="story__pictures">pictures</div>
+        <div className="story__content">content</div>
+        <section className="homes">Homes</section>
+        <section className="gallery">Gallery</section>
+        <footer className="footer">Footer</footer>
+      </div>
+    );
   }
 }
 
---
+++ src/components/Sidebar.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Sidebar = () => <div className="sidebar">Sidebar</div>;
+
+export default Sidebar;
