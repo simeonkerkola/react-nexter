fb0aefeb50d062e3372cb6deffe6112fdfdd62d4
sssmi <simeon.kerkola@gmail.com>
2018-04-17 07:31:19 +0300

    Build features section

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Sidebar from './Sidebar';
 import Header from './Header';
+import Features from './Features';
 
 class LandingPage extends Component {
   render() {
@@ -9,7 +10,7 @@ class LandingPage extends Component {
         <Sidebar />
         <Header />
         <div className="realtors">Top 3 realtors</div>
-        <section className="features">Features</section>
+        <Features />
         <div className="story__pictures">pictures</div>
         <div className="story__content">content</div>
         <section className="homes">Homes</section>
