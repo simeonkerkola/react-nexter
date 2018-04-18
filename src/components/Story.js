033eb0ac6f2677952aa268f8d8514cfcbe4f1373
sssmi <simeon.kerkola@gmail.com>
2018-04-18 13:47:30 +0300

    Finish the pictures part of Story section

--- src/components/Story.js
+++ src/components/Story.js
@@ -3,7 +3,10 @@ import { Link } from 'react-router-dom';
 
 const Story = () => (
   <div className="story">
-    <div className="story__pictures">pictures</div>
+    <div className="story__pictures">
+      <img src="/img/story-1.jpeg" alt="Couple with new home" className="story__img--1" />
+      <img src="/img/story-2.jpeg" alt="New house" className="story__img--2" />
+    </div>
     <div className="story__content">
       <h3 className="heading-3 heading-3--dark">Happy customers</h3>
       <h2 className="heading-2 heading-2--dark">&ldquo;Best decision of our lives&rdquo;</h2>
